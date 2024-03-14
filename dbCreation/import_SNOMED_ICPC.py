df14b1c6212dd2494b4faf48d307112102a6292f
myrtekuipers <66560918+myrtekuipers@users.noreply.github.com>
2024-03-14 11:56:44 +0100

    mapping to database

---
+++ dbCreation/import_SNOMED_ICPC.py
@@ -0,0 +1,30 @@
+import json
+import pandas as pd
+import sqlite3
+
+with open('data/ICPC-SNOMED-20230711.json') as f:
+    data = json.load(f)
+
+mapping = pd.DataFrame(data['group'][0]['element'])
+
+mapping['target'] = mapping['target'].apply(lambda x: x[0]['code'])
+
+def insert_commas(x):
+    str_x = str(x)
+    parts = []
+    while str_x:
+        parts.append(str_x[-3:])
+        str_x = str_x[:-3]
+    return ','.join(parts[::-1])
+
+mapping['target'] = mapping['target'].apply(insert_commas)
+
+mapping = mapping[['target', 'code']]
+
+conn = sqlite3.connect('data/mapping.sqlite3')
+
+mapping.to_sql('mapping', conn, if_exists='replace', index=False)
+
+conn.close()
+
+
